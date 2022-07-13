73a998df41da2798d21365253d22005d1e452c4b
sushmithapaul <109204650+sushmithapaul@users.noreply.github.com>
2022-07-13 16:12:58 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,35 @@
+print("Welcome to Jenkins page")
+def mygen():
+    n = 1
+    yield n
+    n+=2
+    yield n
+    n+=3
+    yield n
+    yield "End"
+for n in mygen():
+    print(n)
+    print('succesful login')
+    print('What is your name?')
+    myName = input()
+    print('it is good to meet you,' + myName)
+    print('The length of your name is:')
+    print(len(myName))
+    print('what is your age?')
+    myAge = input()
+    print('you will be ' + str(int(myAge) + 1) + 'in a year')
+    print('happy seeing you')
+    password = 'markismark'
+    if password == 'ma ark':
+        print('acess')
+    else:
+        print('wrong password')
+
+
+    def hello():
+        print('Hiii')
+        print('Welcome to the Page')
+        print('Thankyou !!')
+
+
+    hello()
